f493df4a512e6bfb333249b4474ba4f9589045fc
aliendev <nasriblog12@gmail.com>
2025-10-06 03:05:47 +0700

    Change server port from 8000 to 55555 in main.py

--- main.py
+++ main.py
@@ -71,4 +71,4 @@ def generate_subdomain(project_name: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=55555, reload=True)
